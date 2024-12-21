9e26f300447da0692e2fd15930f10a0aaa23bf9f
samina-shameem <samina.shameem.clt@gmail.com>
2024-12-21 10:55:04 +0100

    tested.

--- script.js
+++ script.js
@@ -31,26 +31,26 @@ document.getElementById('update-form').addEventListener('submit', function (even
     //console.log("Name:", name, "Age:",age, "Subjects:", subjects);
 
 
-//update the student object with new values
-student.name = name || student.name;
-student.age = age || student.age;
-student.subjects = subjects[0] ? subjects : student.subjects;
+    //update the student object with new values
+    student.name = name || student.name;
+    student.age = age || student.age;
+    student.subjects = subjects[0] ? subjects : student.subjects;
 
 
 
-//update displayed student information
-displayStudentInfo();
+    //update displayed student information
+    displayStudentInfo();
 
-//clear i/p fields
+    //clear i/p fields
 
-document.getElementById('name').value = '';
-document.getElementById('age').value = '';
-document.getElementById('subjects').value = '';
+    document.getElementById('name').value = '';
+    document.getElementById('age').value = '';
+    document.getElementById('subjects').value = '';
 });
 
 //handle the removal of student information
 
-document.getElementById('remove-button').addEventListener('click', function(){
+document.getElementById('remove-button').addEventListener('click', function () {
     student = {};//clear student ojct
     displayStudentInfo();//update displayed info
 });
